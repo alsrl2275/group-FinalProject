473cca64da5060cef2fb56db3a9a0099573361df
qkrwn <qkrwn@DESKTOP-2HFGUP7>
2024-01-25 17:49:26 +0900

    그룹참여하기

--- front/src/JS/images.js
+++ front/src/JS/images.js
@@ -1,7 +1,40 @@
-const images = [
-    "/Image/List/sport1.jpg",
-    "/Image/List/sport2.jpg",
-    "/Image/List/sport3.jpg",
-    "/Image/List/sport4.jpg"
-  ];
-  export default images;
+    const images = {
+      sport: [
+        "/Image/List/sport1.jpg",
+        "/Image/List/sport2.jpg",
+        "/Image/List/sport3.jpg",
+        "/Image/List/sport4.jpg"
+      ],
+      IT: [
+        "/Image/List/IT1.png",
+        "/Image/List/IT2.jpg",
+        "/Image/List/IT3.jpg",
+        "/Image/List/IT4.jpg"
+      ],
+      tour: [
+        "/Image/List/tour1.jpg",
+        "/Image/List/tour2.jpg",
+        "/Image/List/tour3.jpg",
+        "/Image/List/tour4.jpg"
+      ],
+      work: [
+        "/Image/List/work1.jpg",
+        "/Image/List/work2.jpg",
+        "/Image/List/work3.jpg",
+        "/Image/List/work4.jpg"
+      ],
+      life: [
+        "/Image/List/life1.jpg",
+        "/Image/List/life2.jpg",
+        "/Image/List/life3.jpg",
+        "/Image/List/life4.jpg"
+      ],
+      language: [
+        "/Image/List/language1.jpg",
+        "/Image/List/language2.jpg",
+        "/Image/List/language3.jpg",
+        "/Image/List/language4.jpg"
+      ]
+    };
+
+      export default images;
--- front/src/components/Home/Select.jsx
+++ front/src/components/Home/Select.jsx
@@ -1,25 +1,44 @@
 import "../../css/Select.css"; // CSS 파일 불러오기
+import React, { useState } from "react";
 
+const Select = ({ onSearch }) => {
+    const [searchValue, setSearchValue] = useState("");
+    const [selectedValue, setSelectedValue] = useState("all");
+  
+    const handleSearch = () => {
+      // 검색 시 아무것도 선택하지 않았을 경우 "all"로 설정
+      const selected = selectedValue || "all";
+  
+      // 검색 시 아무것도 입력하지 않았을 경우 빈 문자열로 설정
+      const search = searchValue || "all";
+  
+      // 콜백 함수 호출
+      onSearch(search, selected);
+      setSearchValue("");
+      setSelectedValue("all"); // 검색 후에 selectedValue를 초기화
+    };
+  
+    const handleSelectChange = (e) => {
+      setSelectedValue(e.target.value);
+    };
 
-
-
-const Select = () => {
-    return(
-        <div className="Selected">
-            <select className="Selected2">
-                <option key="all" value="all">선택하세요</option>
-                <option key="tour" value="tour">여행</option>
-                <option key="life" value="life">라이프 스타일</option>
-                <option key="sport" value="sport">스포츠/운동</option>
-                <option key="it" value="it">코딩/IT</option>
-                <option key="language " value="language ">어학</option>
-                <option key="employment" value="employment">취업</option>
-            </select>
-            <input className="Inputed" type="text"/>
-            <input className="Inputed2" type="submit" value="검색"/>
-        </div>
+  
+    return (
+      <div className="Selected">
+        <select className="Selected2" onChange={handleSelectChange} value={selectedValue}>
+          <option key="all" value="all">선택하세요</option>
+          <option key="tour" value="tour">여행</option>
+          <option key="life" value="life">라이프 스타일</option>
+          <option key="sport" value="sport">스포츠/운동</option>
+          <option key="IT" value="IT">코딩/IT</option>
+          <option key="language" value="language">어학</option>
+          <option key="work" value="work">취업</option>
+        </select>
+        <input className="Inputed" type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+        <input className="Inputed2" type="submit" value="검색" onClick={handleSearch} />
+      </div>
     );
-
-
-};
-export default Select;
+  };
+  
+  export default Select;
+  
