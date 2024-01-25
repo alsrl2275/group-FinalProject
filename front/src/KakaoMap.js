4a51f3511b6ea5729161a050bc6378a4c164e572
KoreaIT-BKG <dldbstjdzlt@naver.com>
2024-01-25 17:35:27 +0900

    카카오.js

--- front/src/KakaoMap.js
+++ front/src/KakaoMap.js
@@ -1,52 +1,62 @@
+// KakaoMap.js
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../src/css/KakaoMap.css';
 
-function KakaoMap() {
+function KakaoMap({ center }) {
   
-  //스크립트 파일 읽어오기
-  const new_script = src => { 
-    return new Promise((resolve, reject) => { 
-      const script = document.createElement('script'); 
-      script.src = src; 
-      script.addEventListener('load', () => { 
-        resolve(); 
-      }); 
-      script.addEventListener('error', e => { 
-        reject(e); 
-      }); 
-      document.head.appendChild(script); 
-    }); 
-  };
-  
-  useEffect(() => { 
-    //카카오맵 스크립트 읽어오기
-    const my_script = new_script('https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=03676f18b99991f944f886da9fc7de60');
-    
-    //스크립트 읽기 완료 후 카카오맵 설정
-    my_script.then(() => { 
-      console.log('script loaded!!!');  
-      const kakao = window['kakao']; 
+  const [map, setMap] = useState(null);
+  const [marker, setMarker] = useState(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=03676f18b99991f944f886da9fc7de60';
+    script.addEventListener('load', () => {
+      const kakao = window.kakao;
       kakao.maps.load(() => {
         const mapContainer = document.getElementById('map');
-        const options = { 
-          center: new kakao.maps.LatLng(37.50006335700178, 127.03558085159663), //좌표설정
-          level: 3 
-        }; 
-        const map = new kakao.maps.Map(mapContainer, options); //맵생성
-        //마커설정
-        const markerPosition = new kakao.maps.LatLng(37.50006335700178, 127.03558085159663); 
-        const marker = new kakao.maps.Marker({ 
-          position: markerPosition
-        }); 
-        marker.setMap(map); 
-      });   
-    }); 
-  }, []);
+        const initialCenter = center || { lat: 37.50006335700178, lng: 127.03558085159663 };
+        const options = {
+          center: new kakao.maps.LatLng(initialCenter.lat, initialCenter.lng),
+          level: 3,
+
+        };
+        const newMap = new kakao.maps.Map(mapContainer, options);
+
+        
+        // 마커 생성
+        const markerPosition = new kakao.maps.LatLng(initialCenter.lat, initialCenter.lng);
+        const newMarker = new kakao.maps.Marker({
+          position: markerPosition,
+        });
+
+        // 지도에 마커 표시
+        newMarker.setMap(newMap);
+
+        setMap(newMap);
+        setMarker(newMarker);
+      });
+    });
+    document.head.appendChild(script);
+
+    return () => {
+      // 컴포넌트가 언마운트될 때 스크립트 삭제
+      document.head.removeChild(script);
+    };
+  }, [center]);
+
+  useEffect(() => {
+    // center가 변경되면 맵의 중심과 마커 위치 업데이트
+    if (map && marker && center) {
+      const newCenter = new window.kakao.maps.LatLng(center.lat, center.lng);
+      map.setCenter(newCenter);
+      marker.setPosition(newCenter);
+    }
+  }, [map, marker, center]);
 
   return (
     <div className="KakaoMap">
-      <div id="map" className="map"/>
+      <div id="map" className="map" />
     </div>
   );
 }
