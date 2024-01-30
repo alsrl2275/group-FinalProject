5101080c1bd8f42c8a63b0cac8a6524b15c52099
qkrwn <qkrwn@DESKTOP-2HFGUP7>
2024-01-30 13:49:32 +0900

    모달

---
+++ front/src/components/Modal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../css/Modal.css"
+const Modal = ({ isOpen, handleClose, data }) => {
+  // 모달이 열려 있지 않으면 null 반환
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        {/* 모달 내용 18줄까지 가능*/}
+        <h2>{data.seq}</h2>
+        <p>{data.category}</p>
+        <h2>{data.title}</h2>
+        <p>{data.face}</p>
+        <p>{data.members}</p>
+        <p>{data.program}</p>
+        
+
+        {/* 기타 데이터 출력 */}
+        <button className="modal-content-but" onClick={handleClose}>닫기</button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
