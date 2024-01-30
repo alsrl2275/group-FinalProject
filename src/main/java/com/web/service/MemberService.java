ab6df2c4f1d85ebb8575ccfeaeaac208486f3f14
zkflsk <kwonkubi@gmail.com>
2024-01-30 14:25:28 +0900

    chan

--- src/main/java/com/web/service/MemberService.java
+++ src/main/java/com/web/service/MemberService.java
@@ -1,5 +1,22 @@
 package com.web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.web.dto.MemberDTO;
+import com.web.persistence.MemberRepository;
+
 public class MemberService {
+	
+	@Autowired
+	private MemberRepository memberRps;
+
+	public String checkId(String id) {
+		
+		return null;
+	}
+
+	public String signUp(MemberDTO memberDTO) {
+		return null;
+	}
 
 }
