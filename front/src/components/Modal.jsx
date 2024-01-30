83307b7b047e35c4c8592a9437c4363f0356e2c1
qkrwn <qkrwn@DESKTOP-2HFGUP7>
2024-01-30 13:57:33 +0900

    모달 추가

--- front/src/components/Modal.jsx
+++ front/src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/Modal.css"
 const Modal = ({ isOpen, handleClose, data }) => {
-  // 모달이 열려 있지 않으면 null 반환
+  // 모달이 열려 있지 않으면 null 반환할꺼양
   if (!isOpen) {
     return null;
   }
@@ -16,7 +16,7 @@ const Modal = ({ isOpen, handleClose, data }) => {
         <p>{data.face}</p>
         <p>{data.members}</p>
         <p>{data.program}</p>
-        
+        <p>{data.program}</p><p>{data.program}</p>
 
         {/* 기타 데이터 출력 */}
         <button className="modal-content-but" onClick={handleClose}>닫기</button>
