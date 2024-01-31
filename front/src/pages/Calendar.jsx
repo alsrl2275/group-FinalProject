2dd63496730b255938058161836c4f77fa586142
KoreaIT-BKG <dldbstjdzlt@naver.com>
2024-01-31 16:13:54 +0900

    dd

--- front/src/pages/Calendar.jsx
+++ front/src/pages/Calendar.jsx
@@ -3,44 +3,69 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import '../css/Calendar.css'
 import Header from '../components/Home/header';
+import axios from 'axios';
 
 
 class Calendar extends Component {
-    render() {
-        return (
-          <div className="App">
-            <Header/>
-          <div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      groupData: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchGroupData();
+  }
+
+  fetchGroupData = async () => {
+    try {
+      const response = await axios.get('/getGroupData'); // 적절한 엔드포인트를 사용
+      this.setState({ groupData: response.data });
+    } catch (error) {
+      console.error('데이터를 불러오는 중 오류 발생:', error);
+    }
+  };
+
+  render() {
+    const { groupData } = this.state;
+
+    return (
+      <div className="App">
+        <Header />
+        <br />
+        <div className="content-container">
+          <div className="table-container">
             <table>
               <tbody>
-                <th>모임 현황</th>
+                <tr>
+                  <th>모임 현황</th>
+                </tr>
+                {groupData.map((group) => (
+                  <tr key={group.groupId}>
+                    <td>
+                      {group.meetingTitle} - {group.meetingLocation} -{' '}
+                      {group.meetingDateStart}
+                    </td>
+                  </tr>
+                ))}
               </tbody>
-              <tr>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>3</td>
-              </tr>
-              <tr>
-                <td>4</td>
-              </tr>
             </table>
           </div>
-            <br/>
-            <FullCalendar 
-              defaultView="dayGridMonth" 
-              plugins={[ dayGridPlugin ]}
-              events={[
-                { title: '축구모임', date: '2022-09-01'},
-                { title: '데이트', date: '2022-09-02'}
-              ]}
+          <div className="calendar">
+            <FullCalendar
+              defaultView="dayGridMonth"
+              plugins={[dayGridPlugin]}
+              events={groupData.map((group) => ({
+                title: group.meetingTitle,
+                date: group.meetingDateStart,
+              }))}
             />
-            
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
+
 export default Calendar;
--- src/main/java/com/web/controller/GroupController.java
+++ src/main/java/com/web/controller/GroupController.java
@@ -1,7 +1,10 @@
 package com.web.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,7 @@ public class GroupController {
 		groupService.insertGroup(formData);
 		return formData;
 	}
+	
+
 }
 
