a40a2368ae0c905f07757c77e04f4a88a9e5a532
zkflsk <kwonkubi@gmail.com>
2024-02-05 12:58:39 +0900

    0205찬영

--- src/main/java/com/web/dto/MemberDTO.java
+++ src/main/java/com/web/dto/MemberDTO.java
@@ -1,5 +1,6 @@
 package com.web.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,23 +13,38 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 @Entity
-@Table(name="member")
+@ToString
+@Table(name = "member")
 public class MemberDTO {
 	
 	@Id
+	private Long seq;
+	@Column(name = "id")
 	private String id;
+	@Column(name = "pwd")
 	private String pwd;
+	@Column(name = "name")
 	private String name;
+	@Column(name = "email")
 	private String email;
+	@Column(name = "domain")
+	private String domain;
+	@Column(name = "phone")
 	private String phone;
+	@Column(name = "birth")
 	private int birth;
+	@Column(name = "age")
 	private int age;
+	@Column(name = "bank")
 	private String bank;
+	@Column(name = "banknum")
 	private String banknum;
+	@Column(name = "count")
 	private int count;
+	@Column(name = "point")
 	private int point;
+	@Column(name = "role")
 	private String role;
 	
 }
