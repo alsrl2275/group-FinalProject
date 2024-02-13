ce82f921e318c699260da7b7fd886b4a11747d27
qkrwn <qkrwn@DESKTOP-2HFGUP7>
2024-02-13 15:39:53 +0900

    리뷰 완료함

--- src/main/java/com/web/persistence/ReviewRepository.java
+++ src/main/java/com/web/persistence/ReviewRepository.java
@@ -1,9 +1,13 @@
 package com.web.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.dto.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>{
+
+	List<Review> findByLeaderId(String id);
 
 }
